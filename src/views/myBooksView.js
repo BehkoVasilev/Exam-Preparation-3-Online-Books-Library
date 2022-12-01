fc306184fe981820df5a03394313efd20aca45be
BehkoVasilev <vencislavvsilev@gmail.com>
2022-12-01 08:50:03 +0200

    .

--- src/views/myBooksView.js
+++ src/views/myBooksView.js
@@ -1,27 +1,28 @@
 import { html, nothing } from "../lib.js";
 import { getAllBooks } from "../api/data.js";
 
-const myBooksTemplate = (hasBookList, cardBookTemplate) => html`<section id="my-books-page" class="my-books">
-<h1>My Books</h1>
-<ul class="my-books-list">
-    ${user ? html`
-    <li class="otherBooks">
-        ${hasBookList.length > 0 ? html`${hasBookList.map(cardBookTemplate)}` : 
-            html`<p class="no-books">No books in database!</p>`}
-    </li>`: 
+const myBooksTemplate = (hasBookList, cardBookTemplate) => html`
+<section id="my-books-page" class="my-books">
+    <h1>My Books</h1>
+    <!-- Display ul: with list-items for every user's books (if any) -->
+    ${user ? html` 
+        <ul class="my-books-list">
+            ${hasBookList.length > 0 ? 
+                html`
+                ${hasBookList.map(cardBookTemplate)}` : 
+                html`<p class="no-books">No books in database!</p>`}
+        </ul>` : 
     html`<p class="no-books">No books in database!</p>`}
-    
-    
-</ul>
-</section>`
+</section>`;
 
 
 const cardBookTemplate = (book) => html`
+<li class="otherBooks">
     <h3>${book.title}</h3>
-    <p>Type: ${book.type}</p>
-    <p class="img"><img src=${book.imageUrl}></p>
+    <p>Type: >${book.type}</p>
+    <p class="img"><img src=>${book.imageUrl}></p>
     <a class="button" href="/details/${book._id}">Details</a>
-`;
+</li>`;
 
 export async function showMyBooks(ctx) {
     const user = ctx.user;
